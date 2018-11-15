8501b94e89d81c7ee9282139e781f2bd6b997625
Mahmoud Agamy <agamyv@gmail.com>
2018-11-15 09:05:19 +0000

    automatically backed up by learn

--- spec/array_crud_spec.rb
+++ spec/array_crud_spec.rb
@@ -1,5 +1,5 @@
 require "./spec_helper"
-require_relative "../array_crud.rb"
+require_relative "../lib/array_crud.rb"
 
 describe "#create_an_empty_array" do
   it "creates and returns and empty array" do 
